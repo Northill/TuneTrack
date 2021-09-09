3567f015bf506412d34fab8692d15735680edeeb
Thomas Houghton <tomhoughton888@gmail.com>
2021-09-09 19:44:32 +0100

    Created Login Screen

--- tune-tracker/src/Components/Login.js
+++ tune-tracker/src/Components/Login.js
@@ -1,3 +1,6 @@
+// CSS:
+import '../App.css';
+
 // React:
 import React, { Component } from 'react';
 
@@ -18,11 +21,20 @@ class Login extends Component {
     render() {
         return (
             <>
-             <Card>
-                <Card.Title align='center'>Welcome to Tune Track</Card.Title>
-                <OAuthLoginMethods />
-                <LoginForm />
-             </Card>
+                <div className='tDiv'>
+                <Card className='tCard'>
+                    <Card.Title id='loginTitle' align='center'>Tune Track</Card.Title>
+                    <Row>
+                        <Col></Col>
+                        <Col>
+                            <img id='logo' src='/tuneLogo.svg' />
+                        </Col>
+                        <Col></Col>
+                    </Row>
+                    <LoginForm />
+                    <Button id='loginButton'>Login</Button>
+                </Card>
+                </div>
             </>
         );
     }
@@ -34,16 +46,9 @@ const OAuthLoginMethods = () => {
     return (
         <Row align='center'>
             <Col>
-                <Button>oAUth</Button>
-            </Col>
-            <Col>
-                <Button>oAUth</Button>
-            </Col>
-            <Col>
-                <Button>oAUth</Button>
-            </Col>
-            <Col>
-                <Button>oAUth</Button>
+                <div>
+                    <img src='/googleAuth.png' />
+                </div> 
             </Col>
         </Row>
     )
@@ -53,23 +58,15 @@ const LoginForm = () => {
     return (
         <Form>
             <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Control className='tFormText' type="email" placeholder="Enter email" />
                 <Form.Text className="text-muted">
                 We'll never share your email with anyone else.
                 </Form.Text>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Control className='tFormText' type="password" placeholder="Password" />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-                Submit
-            </Button>
         </Form>
     )
 }
